20ddaaee44d55af4892d71cd5035b76b0bdd3338
lexiscode <nwokoriealex20@gmail.com>
2022-09-26 08:38:08 -0700

    Added tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  set_string - a function that sets the value of a pointer to a char
- *  @s: double pointer to a string
- *  @to: pointer to the string to set s to
- *
- *  Return: Description of the returned value
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ * @to: string
+ * Return: void
  */
+
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
