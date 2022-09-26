3b07823eea8e275f32c76c56eb7522d6ccdc0145
lexiscode <nwokoriealex20@gmail.com>
2022-09-26 08:08:47 -0700

    Added project tasks

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,29 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chess board
+ *  print_chessboard - a function that prints a chessboard
+ *  @a: an array input to print
  *
- * @a: input pointer
- *
- * Return: return nothing
-*/
+ *  Return: Nothing
+ */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+int i = 0, j;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
-	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
+for (; i < 8; i++)
+{
 
-		_putchar(a[i / 8][i - j]);
-		i++;
-	}
-	_putchar('\n');
+for (j = 0; j < 8; j++)
+_putchar(a[i][j]);
+_putchar('\n');
+}
 }
