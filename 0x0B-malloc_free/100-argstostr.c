9002bbcc85293d81be79ba338391bf8e796d4af0
lexiscode <nwokoriealex20@gmail.com>
2022-10-03 05:09:32 -0700

    Added tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,34 +2,38 @@
 #include "main.h"
 
 /**
- * *argstostr - concatenates all the arguments of the program
- * @ac: number of arguments
- * @av: array of arguments
+ * argstostr - a function that concatenates
+ *             all the arguments
  *
- * Return: Pointer to the new string (Success), NULL (Error)
- */
+ * @ac: argument counter
+ * @av: argument holder
+ *
+ * Return: a pointer to a new string
+ *         or NULL if it fails
+*/
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
+	int i, j, k, length;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+			length++;
+		length++;
 	}
 
-	str = malloc(sizeof(char) * (len + 1));
-
+	str = malloc((length + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 
 	k = 0;
-
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -40,6 +44,7 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
+	str[k] = '\0';
 
 	return (str);
 }
